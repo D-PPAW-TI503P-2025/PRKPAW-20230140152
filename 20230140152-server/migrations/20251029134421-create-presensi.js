8f9f68bb119a310138fd7f858624299b04c44432
muhammadrayhanwidayat <murayat99@gmail.com>
2025-10-29 20:44:40 +0700

    depedensi baru

---
+++ 20230140152-server/migrations/20251029134421-create-presensi.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Presensis', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      nama: {
+        type: Sequelize.STRING
+      },
+      checkIn: {
+        type: Sequelize.DATE
+      },
+      checkOut: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Presensis');
+  }
+};
---
+++ 20230140152-server/models/presensi.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Presensi extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Presensi.init({
+    userId: DataTypes.INTEGER,
+    nama: DataTypes.STRING,
+    checkIn: DataTypes.DATE,
+    checkOut: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Presensi',
+  });
+  return Presensi;
+};
