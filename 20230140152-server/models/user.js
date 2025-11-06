a210d001285d5f878f99a5f0849c404893b1036e
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-06 14:19:16 +0700

    walaupun belum waktunya mengerjakan tapi saia mengambil langkah lebih depan itulah rayhan awokaowkaowkwoo

---
+++ 20230140152-server/models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // Contoh relasi:
+      // User.hasMany(models.Presensi, { foreignKey: 'userId' });
+    }
+  }
+
+  User.init({
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('mahasiswa', 'admin'),
+      allowNull: false,
+      defaultValue: 'mahasiswa'
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+
+  return User;
+};
--- 20230140152-server/server.js
+++ 20230140152-server/server.js
@@ -6,22 +6,32 @@ const morgan = require("morgan")
 // Impor router
 const presensiRoutes = require("./routes/presensi");
 const reportRoutes = require("./routes/reports");
+const ruteBuku = require("./routes/books");
+const authRoutes = require('./routes/auth');
+
+
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //untuk parsing JSON body
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
+
 app.get("/", (req, res) => {
   res.send("Home Page for API");
 });
-const ruteBuku = require("./routes/books");
+
+// route setelah middleware
 app.use("/api/books", ruteBuku);
 app.use("/api/presensi", presensiRoutes);
 app.use("/api/reports", reportRoutes);
+app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
+
+
